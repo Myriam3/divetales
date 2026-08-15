52a590efe34d3a3cc776833e5d78bcc553997b90
Myriam Bucourt <myriam.bucourt@gmail.com>
2026-08-15 10:49:08 +0900

    Dives index

--- app/controllers/dives_controller.rb
+++ app/controllers/dives_controller.rb
@@ -1,6 +1,10 @@
 class DivesController < ApplicationController
   def index
-    @dives = policy_scope(Dive).order(:date)
+    @dives = policy_scope(Dive).order(date: :desc)
+
+    @countries_count = @dives.map { |dive| dive.location.country }.compact.uniq.count
+    @pictures_count = @dives.flat_map(&:pictures).uniq.count
+    @species_count = @dives.flat_map { |dive| dive.pictures.flat_map(&:species) }.uniq.count
   end
 
   def new
