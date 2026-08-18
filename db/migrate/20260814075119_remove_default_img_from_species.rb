f828aa38e917f379efb335a3f7a4575aa3025b04
Celeste Zhu <celestezhuyy@gmail.com>
2026-08-18 20:01:17 +0900

    Fix remove default_img migration

--- db/migrate/20260814075119_remove_default_img_from_species.rb
+++ db/migrate/20260814075119_remove_default_img_from_species.rb
@@ -1,5 +1,5 @@
 class RemoveDefaultImgFromSpecies < ActiveRecord::Migration[8.1]
   def change
-    remove_column :species, :default_img, :string
+    remove_column :species, :default_img, :string, if_exists: true
   end
 end
