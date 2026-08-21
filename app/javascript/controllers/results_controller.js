4da822d7dc04484eca640fbadb4897d4c27b4aa4
Celeste Zhu <celestezhuyy@gmail.com>
2026-08-21 16:09:13 +0900

    hide possible matches when adding more context to rerun

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["tab", "content"]
+  static targets = ["tab", "content", "matchesContainer", "refineForm"]
 
   show(event) {
     const clickedTab = event.currentTarget
@@ -20,5 +20,19 @@ export default class extends Controller {
     // Activate clicked tab
     clickedTab.classList.add("active")
     this.contentTargets[index].classList.remove("d-none")
+  }
+
+  toggleForm(event) {
+    this.refineFormTarget.classList.toggle("d-none")
+
+    if (this.refineFormTarget.classList.contains("d-none")) {
+      this.matchesContainerTarget.classList.remove("d-none")
+      event.target.innerText = "Add more context & try again"
+      event.target.classList.replace("btn-outline-danger", "btn-outline-primary")
+    } else {
+      this.matchesContainerTarget.classList.add("d-none")
+      event.target.innerText = "Cancel / Back to matches"
+      event.target.classList.replace("btn-outline-primary", "btn-outline-danger")
+    }
   }
 }
