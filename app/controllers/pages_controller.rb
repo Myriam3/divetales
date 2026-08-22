504a691b9e13b83eade9a4e5d04418ed59ee4470
Celeste Zhu <celestezhuyy@gmail.com>
2026-08-22 16:53:29 +0900

    fixed latest species logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,18 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @latest_species = Species.with_attached_default_photo.includes(:pictures,
-                                                                   dives: :location).order(created_at: :desc).limit(5)
+    latest_species_ids = PictureSpecy
+                         .joins(picture: { dive: :trip })
+                         .where(trips: { user_id: current_user.id })
+                         .order(created_at: :desc)
+                         .pluck(:species_id)
+                         .uniq
+                         .first(5)
+
+    @latest_species = Species
+                      .where(id: latest_species_ids)
+                      .with_attached_default_photo
+                      .includes(:pictures, dives: :location)
     @media_gallery = Dive.joins(:pictures).includes(pictures: { photo_attachment: :blob }).order(date: :desc).distinct.limit(10)
   end
 end
