c232594ce8aa63938d76134748d66f239aebd2e8
Celeste Zhu <celestezhuyy@gmail.com>
2026-08-22 13:59:35 +0900

    added instance variable in pagescontroller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @latest_species = Species.includes(dive: :location).order(created_at: :desc).limit(5)
+    @dives_with_media = Dive.joins(:photos_attachments).order(date: :desc).limit(10)
   end
 end
