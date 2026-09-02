7ee30dab0198a94c0a75141c6787bf5a41224bde
Celeste Zhu <celestezhuyy@gmail.com>
2026-09-02 20:36:54 +0800

    deleted bubbles in detail

--- app/javascript/controllers/navbar_scroll_controller.js
+++ app/javascript/controllers/navbar_scroll_controller.js
@@ -8,16 +8,13 @@ export default class extends Controller {
   handleScroll() {
     const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    // Scroll Down: Hide navbar (only if scrolled past 80px to avoid glitching at the very top)
     if (currentScrollTop > this.lastScrollTop && currentScrollTop > 80) {
       this.element.classList.add("navbar--hidden");
     }
-    // Scroll Up: Show navbar
     else if (currentScrollTop < this.lastScrollTop) {
       this.element.classList.remove("navbar--hidden");
     }
 
-    // Prevent negative values when elastic-bouncing at the top on Safari/iOS
     this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
   }
 }
